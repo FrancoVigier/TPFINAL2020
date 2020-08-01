e0d07474423ff26ddac749463aaf1ef63c8c21f4
FrancoVigier <francovallejosvigier@gmail.com>
2020-07-31 23:56:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -378,7 +378,7 @@ int main() {
             destruir_conjunto(C,NULL);
             destruir_conjunto(D,NULL);
         
-            dlist_destruir(uni,NULL);
+            dlist_destruir(uni,(Visitante)free_int_punt);
         interprete = 1;
 
         break;
