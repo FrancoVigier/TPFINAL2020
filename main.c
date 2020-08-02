c54a84eeb9c0e54906653ee8ff6715960cec6043
FrancoVigier <francovallejosvigier@gmail.com>
2020-08-02 20:00:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -560,6 +560,7 @@ int main() {
           Conjunto complemento = crear_conjunto("carlos", NULL, complemetos);
           hash_inserto(HASH, "carlos", NULL, complemetos);
           mostrar_conjunto(complemento);
+          freeLista = prepend_glist(freeLista,complemento);
         } else {
           printf("\nUno de los operandos no existe...\n");
         }
