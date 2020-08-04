f875204d4084f101d6e91411c4e7fedab583599c
FrancoVigier <francovallejosvigier@gmail.com>
2020-08-04 16:30:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -498,7 +498,7 @@ int main() {
 
         if(test == 1){
         char alias[] = "pepe";
-        char operacion[] = " {x : 25 <= x <= 75}";
+        char operacion[] = " {x : 100 <= x <= 150}";
         printf("-%s- , -%s-", operacion, alias);
         Conjunto operandoB = definir_conj_com(operacion, alias);//hashea operando con el alias
         hash_inserto(HASH, operandoB->alias, operandoB->lista, operandoB->intervaloLista);
