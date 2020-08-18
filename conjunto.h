4bf257e83ee8225ba9f7106c986e867712fe81b8
FrancoVigier <francovallejosvigier@gmail.com>
2020-08-18 17:50:27 -0300

    Update conjunto.h

--- conjunto.h
+++ conjunto.h
@@ -5,11 +5,39 @@
 #include "parser.h"
 #include "operaciones.h"
 
+/**
+* Inicializamos un conjunto
+*/
 Conjunto inicializar_conjunto(char* alias, char* operacion);
+
+/**
+* Creamos un conjunto por listas por copia
+*/
 Conjunto crear_conjunto(char* alias, GList lista, GList intervaloLista);
+
+/**
+* Dada una definicion por comprension creo un intervalo
+* y lo cargo a un Conjunto parametro
+*/
 void extraer_com_conjunto(char* operacion, Conjunto conjunto);
+
+/**
+* Defino un conjunto por comprension
+*/
 Conjunto definir_conj_com(char* operacion, char* alias);
+
+/**
+* Definimos un conjunto por extension
+*/
 Conjunto definir_conj_ext(char* operacion, char* alias);
+
+/**
+* Imprimimos un conjunto
+*/
 void mostrar_conjunto_imprimir(Conjunto muestro);
+
+/**
+* Liberamos la memoria de un Conjunto
+*/
 void destruir_conjunto(Conjunto conjunto);
 #endif // CONJUNTO_H_INCLUDED
