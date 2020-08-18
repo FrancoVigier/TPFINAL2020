16a1128202ec822d7a179b3416326ab7c6bc5ebb
FrancoVigier <francovallejosvigier@gmail.com>
2020-08-18 17:22:22 -0300

    listo intervalo.h

--- intervalo.h
+++ intervalo.h
@@ -11,6 +11,9 @@ typedef struct _Intervalo {
   long long cardinalidad;
 } Intervalo;
 
+/**
+* Extraemos los intervalos inf y sup de la def. por comprension
+*/
 Intervalo* extraer_ini_y_fin(char* operacion);
 
 #endif // INTERVALO_H_INCLUDED
