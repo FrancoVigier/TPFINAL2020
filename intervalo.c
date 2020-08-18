ed52b716e8bbfdfd15bd96818f24436c6a163b29
FrancoVigier <francovallejosvigier@gmail.com>
2020-08-18 17:21:54 -0300

    listo intervalo.c

--- intervalo.c
+++ intervalo.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "intervalo.h"
-//Extraemos los intervalos inf y sup de la def. por comprension
+
+/**
+* Extraemos los intervalos inf y sup de la def. por comprension
+*/
 Intervalo* extraer_ini_y_fin(char* operacionA) {
   char* operacion = malloc(sizeof(char) * LIMITE);
   strcpy(operacion,operacionA);
   char separadorInicial[] = " ";
-//Dividimos la cadena por espacios en blanco
+  //Dividimos la cadena por espacios en blanco
   char *fraccion = strtok(operacion, separadorInicial);
   char *inicio = NULL;
   char *fin = NULL;
   int contador = 0;
   if (fraccion != NULL) {
     while (fraccion != NULL) {
-//Obtengo las cadenas que representan los extremos
+  //Obtengo las cadenas que representan los extremos
       if (contador == 2)
         inicio = fraccion;
       if (contador == 6)
@@ -24,10 +27,10 @@ Intervalo* extraer_ini_y_fin(char* operacionA) {
     }
   }
   fin[strlen(fin) - 1] = '\0';
-//Convierto las cadenas a numeros
+  //Convierto las cadenas a numeros
   long long init = numero_char_a_long_long(inicio);
   long long fina = numero_char_a_long_long(fin);
-//Chekeo si el conjunto declarado es vacio o no
+  //Chekeo si el conjunto declarado es vacio o no
   if (fina < init) {
     free(operacion);
     return NULL;
@@ -38,7 +41,7 @@ Intervalo* extraer_ini_y_fin(char* operacionA) {
   intervalo->ultimo = fina;
   intervalo->esVacio = 0;
   intervalo->cardinalidad = 0;
-//Calculo la cardinalidad del intervalo declarado
+  //Calculo la cardinalidad del intervalo declarado
   if (intervalo->esVacio == 0 && intervalo->inicio >= 0 &&
       intervalo->ultimo >= 0) {
     intervalo->cardinalidad = intervalo->ultimo - intervalo->inicio;
@@ -54,6 +57,6 @@ Intervalo* extraer_ini_y_fin(char* operacionA) {
     intervalo->cardinalidad = intervalo->ultimo - intervalo->inicio;
     intervalo->cardinalidad++;
   }
-  free(operacion);
+  free (operacion);
   return intervalo;
 }
