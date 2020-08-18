fe2e25a62a6e11395b8e2c5ec6244e6f9456685d
FrancoVigier <francovallejosvigier@gmail.com>
2020-08-18 17:35:55 -0300

    Update pila.h

--- pila.h
+++ pila.h
@@ -5,10 +5,33 @@ typedef struct _Stack {
   GList primero;
 }* Stack;
 
+/**
+* Inicializamos una nueva pila
+*/
 Stack stack_new();
+
+/**
+* Verificamos que la pila sea vacia
+*/
 int stack_es_vacio(Stack stack);
+
+/**
+* Agregamos un elemento a la pila
+*/
 void stack_push(Stack stack, void *dato);
+
+/**
+* Sacamos el tope de pila
+*/
 void stack_pop(Stack stack);
+
+/**
+* Devolvemos el dato del tope de pila
+*/
 void* stack_top(Stack stack);
+
+/**
+* Destruimos la pila
+*/
 void stack_destruir(Stack stack);
 #endif // PILA_H_INCLUDED
